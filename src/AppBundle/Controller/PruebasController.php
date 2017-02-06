df7c857b4f8bd17d39a3d88314351b5b686cceea
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-02-06 11:22:41 -0500

    Variables, condicionales y bucles en Twig
    Agrega un par de 'Arrays' de datos estáticos (productos y frutas)
    en el controlador, así también dos variables con los titulos para
    el listado de productos y frutas
    
    Se agregan los títulos de listado de productos y se establecen
    algunos condicionales y ciclos para el despliegue de los datos

--- src/AppBundle/Controller/PruebasController.php
+++ src/AppBundle/Controller/PruebasController.php
@@ -11,19 +11,27 @@ use Symfony\Component\HttpFoundation\Request;               # Http
 class PruebasController extends Controller {
 
   public function indexAction( Request $request, $name, $page ) {
-
-    # Redireccion al homepage
-    /*return $this -> redirect(
-      $request -> getBasePath() . '/hello-world?saludo=Hola'
-    );*/
-
-    /*var_dump( $request -> query -> get( 'hola' ) );   # Recoger variables tipo GET (a através de la URL)
-    var_dump( $request -> get( 'hola-post' ) );   # Recoger variables tipo GET o POST ('hola-post' es POST)
-    die();*/
-
+    # Datos estáticos de productos electrónicos
+    $productos = array(
+      array( 'nombre' => 'PlayStation 4',         'precio' => 500 ),
+      array( 'nombre' => 'Samsung Galaxy Note 3', 'precio' => 650 ),
+      array( 'nombre' => 'AsusZendPad C7.0',      'precio' => 150 ),
+      array( 'nombre' => 'SmartTV Samsung 32"',   'precio' => 350 ),
+      array( 'nombre' => 'Battery Varta 9v',      'precio' => 11 )
+    );
+    # Datos estáticos de productos perecederos
+    $frutas = array(
+      'manzana' => 'golden',
+      'naranja' => 'tangelo',
+      'uva'     => 'chilena'
+    );
     // replace this example code with whatever you need
     return $this->render('AppBundle:Pruebas:index.html.twig', array(
-      'texto' => 'indexAction(): ' .$name. ' - ' .$page
+      'texto'     => 'indexAction(): ' .$name. ' - ' .$page,
+      'titulo_productos' => 'Listado productos electrónicos',
+      'productos' => $productos,
+      'titulo_frutas' => 'Listado de frutas',
+      'frutas'    => $frutas
     ));
   }
 
