33baf162ff087dd5dfd4a25733a85a43cc1e08cf
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-02-13 10:16:57 -0500

    Despliega datos en la funcion personalizada
    Importa las los recursos de la extensión de Twig para no agregar
    rutas en las instancias.
    
    Refactoriza la funcionalidad pasando como parámetro para el despliegue
    y las dimensiones de la tabla un 'Array' con valores.

--- src/AppBundle/Twig/HelperVistas.php
+++ src/AppBundle/Twig/HelperVistas.php
@@ -1,21 +1,28 @@
 <?php
   namespace AppBundle\Twig;
 
-  class HelperVistas extends \Twig_Extension {
+  use \Twig_Extension;
+  use \Twig_Function_Method;
+
+  class HelperVistas extends Twig_Extension {
 
     public function getFunctions() {
       return array(
-        'generateTable' => new \Twig_Function_Method( $this, 'generateTable' )
+        'generateTable' => new Twig_Function_Method( $this, 'generateTable' )
       );
     }
 
     # Función Twig, para generar una tabla en la vista
-    public function generateTable( $num_columns, $num_rows ) {
+    public function generateTable( $resultSet ) {
+
+      # echo '<pre>'; print_r( $resultSet ); echo '</pre>'; exit();
+
       $table = '<table class="table" border="1">';
-      for( $i = 0; $i < $num_rows; $i++ ) {
+      for( $i = 0; $i < count( $resultSet ); $i++ ) {
         $table .= '<tr>';
-        for( $j = 0; $j < $num_columns; $j++ ) {
-          $table .= '<td>Columna</td>';
+        for( $j = 0; $j < count( $resultSet[ $i ] ); $j++ ) {
+          $resultSetValues = array_values( $resultSet[ $i ] );    # convertimos el 'Array' asociativo en uno numérico
+          $table .= '<td>' .$resultSetValues[ $j ]. '</td>';
         }
         $table .= '</tr>';
       }
