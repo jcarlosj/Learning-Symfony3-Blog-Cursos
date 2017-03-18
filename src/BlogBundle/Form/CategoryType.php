683ec7456bf338d463809b3d6efdced28bdc5214
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2017-03-18 13:28:17 -0500

    CRUD Categorías
    Crea formulario para agregar categorías usando el comando
    de Doctrine para generarlo automáticamente
    
       php bin/console doctrine:generate:form BlogBundle:Category
    
     - Agrega: tipos, etiquetas, atributos y restricciones a la
       configuración tipos del mismo a la clase 'CategoryType'.
     - Crea funcionalidad para insertar registros en los métodos
       'addAction' y 'create' del controlador 'CategoryController'
     - Crea la vista para para el despliegue del formulario de
       agregar categorias en 'add.html.twig'
     - Crea la ruta de acceso a la funcionalidad en el 'routing'
       del 'Bundle'
    
    Lista de categorías
    
     - Crea la funcionalidad para listar registros en el método
       'indexAction' del controlador 'CategoryController'
     - Crea la vista para el despliegue de la lista en
       'index.html.twig'
     - Crea la ruta de acceso a la funcionalidad en el 'routing'
       del 'Bundle'
    
    Eliminar categoría
    
     - Crea la funcionalidad para eliminar los registros en el método
       'deleteAction' del controlador 'CategoryController'
     - Crea la ruta de acceso a la funcionalidad en el 'routing'
       del 'Bundle'
    
    Editar categoría
    
     - Crea la funcionalidad para editar los registros en los métodos
       'editAction' y 'edit' del controlador 'CategoryController'
     - Crea la vista para el despliegue del formulario para editar
       categorías en 'edit.html.twig'
     - Crea la ruta de acceso a la funcionalidad en el 'routing'
       del 'Bundle'

---
+++ src/BlogBundle/Form/CategoryType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class CategoryType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            -> add( 'name', TextType :: class, array(         # Nombre y tipo
+              'label' => 'Nombre',                            # Nombre etiqueta
+              'attr'     => array(                            # Atributos del tag
+                'class' => 'form-name form-control'           # Clase CSS
+              ),
+              'required' => 'required'                        # Campo requerido
+            ))
+            -> add( 'description', TextareaType :: class, array(   # Nombre y tipo
+              'label' => 'Descripción',                            # Nombre etiqueta
+              'attr'     => array(                                 # Atributos del tag
+                'class' => 'form-description form-control'         # Clase CSS
+            )))
+            -> add( 'Guardar', SubmitType :: class, array(    # Nombre y tipo
+              'attr'     => array(                            # Atributos del tag
+                'class' => 'form-submit btn btn-success'      # Clase CSS
+            )))
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BlogBundle\Entity\Category'
+        ));
+    }
+}
